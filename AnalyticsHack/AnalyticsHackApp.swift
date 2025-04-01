71e478a12d4fdc06bde885a370fe0c4cc87006a8
jeremy lehman <jeremy.lehman@onxmaps.com>
2025-04-01 13:39:34 -0400

    Initial Commit

---
+++ AnalyticsHack/AnalyticsHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsHackApp.swift
+//  AnalyticsHack
+//
+//  Created by Jeremy Lehman on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnalyticsHack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnalyticsHack
+//
+//  Created by Jeremy Lehman on 4/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
